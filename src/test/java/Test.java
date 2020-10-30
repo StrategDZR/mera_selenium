c0c62427778928b7adfc91d3d70c15a21f5453ce
Alexey Shein <alexey.shein@revelsystems.com>
2020-10-30 09:31:41 +0300

    add checkPageTitle test

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,5 +1,31 @@
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 public class Test {
-    public static void main(String[] args) {
-        System.out.println("hello world");
+    private WebDriver driver;
+    private final String BASIC_URL = "https://google.com";
+
+
+    @Before
+    public void tierUp(){
+        driver = new ChromeDriver( new ChromeOptions().addArguments("--incognito"));
     }
+
+    @org.junit.Test
+    public void checkPageTitle(){
+        driver.get(BASIC_URL);
+        String title = driver.getTitle();
+        Assert.assertEquals("Google", title);
+    }
+
+    @After
+    public void tierDown(){
+        driver.close();
+        driver.quit();
+    }
+
 }
