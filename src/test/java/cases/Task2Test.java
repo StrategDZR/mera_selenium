58b646b8d95359d3b80e5513ac7e4392af86b8ba
Alexey Shein <alexey.shein@revelsystems.com>
2020-11-05 18:32:44 +0300

    move actions to POM files

--- src/test/java/cases/Task2Test.java
+++ src/test/java/cases/Task2Test.java
@@ -13,12 +13,11 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import pages.AdminMainPage;
 import pages.LoginPage;
+
 import java.util.concurrent.TimeUnit;
 
 public class Task2Test {
     private static final String BASIC_URL = "http://localhost/litecart/admin/";
-    private static final String ADMIN_LOGIN = "admin";
-    private static final String ADMIN_PASS = "admin";
     private WebDriver driver;
     private WebDriverWait wait;
 
@@ -30,21 +29,18 @@ public class Task2Test {
         driver = new ChromeDriver(new ChromeOptions().addArguments("--incognito"));
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         wait = new WebDriverWait(driver, 10);
-        loginPage = new LoginPage();
-        adminMainPage = new AdminMainPage();
+        loginPage = new LoginPage(driver);
+        adminMainPage = new AdminMainPage(driver);
     }
 
     @Test
     public void testLoginToAdmin(){
+        //следующие две строки тоже вынесу в отдельный класс-хелпер в следующем задании
         driver.navigate().to(BASIC_URL);
         wait.until(ExpectedConditions.elementToBeClickable(By.xpath(loginPage.loginButton)));
-        driver.findElement(By.xpath(loginPage.usernameInput)).sendKeys(ADMIN_LOGIN);
-        driver.findElement(By.xpath(loginPage.passwordInput)).sendKeys(ADMIN_PASS);
-        driver.findElement(By.xpath(loginPage.loginButton)).click();
 
-        wait.until(ExpectedConditions.elementToBeClickable(By.xpath(adminMainPage.logoutButton)));
-        WebElement logoutButton = driver.findElement(By.xpath(adminMainPage.logoutButton));
-        Assert.assertTrue(logoutButton.isDisplayed());
+        loginPage.loginAsAdmin();
+        adminMainPage.checkAdminPageIsOpened();
     }
 
     @After
