5305e5eeff36c30878d9188fbcb40e5e6f2c20eb
Alexey Shein <alexey.shein@revelsystems.com>
2020-11-06 09:26:21 +0300

    optimize imports

--- src/test/java/cases/Task2Test.java
+++ src/test/java/cases/Task2Test.java
@@ -1,12 +1,10 @@
 package cases;
 
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
