f0b2b1642e53be113df5d0d3bdc7f0a187880758
Alexey Shein <alexey.shein@revelsystems.com>
2020-11-11 22:02:56 +0300

    add files

---
+++ src/test/java/pages/ShopMainPage.java
@@ -0,0 +1,18 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class ShopMainPage {
+    private WebDriver driver;
+
+    //Locators
+
+
+
+
+    public ShopMainPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+
+}
